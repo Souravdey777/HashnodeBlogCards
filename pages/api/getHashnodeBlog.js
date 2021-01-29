b9ff752edaab4e5fbdced9f31cd4c4d885e5cb33
Souravdey777 <piyush.kolkata@gmail.com>
2021-01-29 19:05:29 +0530

    ✨ dark theme added

--- pages/api/getHashnodeBlog.js
+++ pages/api/getHashnodeBlog.js
@@ -13,13 +13,14 @@ export default async (req, res) => {
         // console.log(hostname, pathname)
         const slug = pathname.replace("/", "");
         const large = req.query.large === "true" ? true : false
+        const dark = req.query.dark === "true" ? true : false
         const resultData = (await getHashnodeBlog(slug, hostname));
         if (!resultData.data.post) {
             res.write(JSON.stringify({ error: 'Post does not exits!' }));
             res.end();
             return;
         }
-        const blogCardObj = await hashnodeBlogCard(resultData.data.post, hostname, large);
+        const blogCardObj = await hashnodeBlogCard(resultData.data.post, hostname, large, dark);
         res.writeHead(200, { 'Content-Type': 'image/svg+xml' });
         res.write(blogCardObj);
         res.end();
--- utils/hashnodeBlogCard.js
+++ utils/hashnodeBlogCard.js
@@ -1,13 +1,53 @@
 import getBase64 from "./getBase64";
 
-const hashnodeBlogCard = async (data, hostname, large = false) => {
-    const { title, dateAdded, brief, author, slug } = data
-    const coverImage = data.coverImage !== "" ? await getBase64(data.coverImage) : "";
-    const profileImage = await getBase64(author.photo);
-    const blogDate = new Date(Date.parse(dateAdded)).toLocaleString('default', { year: 'numeric', month: 'short', day: 'numeric' })
-    const blogLink = `https://${hostname}/${slug}`;
-
+const hashnodeBlogCard = async (data, hostname, large = false, dark = false) => {
+  const { title, dateAdded, brief, author, slug } = data
+  const coverImage = data.coverImage !== "" ? await getBase64(data.coverImage) : "";
+  const profileImage = await getBase64(author.photo);
+  const blogDate = new Date(Date.parse(dateAdded)).toLocaleString('default', { year: 'numeric', month: 'short', day: 'numeric' })
+  const blogLink = `https://${hostname}/${slug}`;
+  if (dark === true) {
     return `<svg  ${large ? ` width="302" height="522"` : ` width="174" height="300" `} viewBox="0 0 302 522" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <a href="${blogLink}" target="_blank">
+    <rect x="1" y="1" width="300" height="520" rx="4" fill="#232626" stroke="#AEAEAE" stroke-width="2"/>
+    <rect id="profileImage" x="16" y="20" width="40" height="40" rx="20" />
+    <clipPath id="profileImageClip">
+    <use xlink:href="#profileImage"/>
+    </clipPath>
+    <image x="16" y="20" width="40" height="40" clip-path="url(#profileImageClip)" preserveAspectRatio="xMidYMid slice" xlink:href="data:image/png;base64,${profileImage}"/>
+    <text fill="#F5F5F5"  font-family="Helvetica" font-size="14" letter-spacing="0em" x="71" y="38"><tspan font-weight="bold">${author.name}</tspan>
+    <tspan fill="#AEAEAE" fill-opacity="0.75"  font-family="Helvetica" font-weight="bold" font-size="14" letter-spacing="0em">@${author.username}</tspan></text>
+    <text fill="#AEAEAE" fill-opacity="0.75"  font-family="Helvetica" font-size="14" font-weight="bold" letter-spacing="0em"><tspan x="71" y="53.8636">${blogDate}</tspan></text>
+    <foreignObject
+          x="16" y="80" width="260" height="120">
+          <p
+            xmlns="http://www.w3.org/1999/xhtml"
+            xlink:href="#blogName"
+            style="color:#F5F5F5;font-family:Helvetica;font-size:20px;font-weight:bold;margin:0;padding:0;font-weight=bold"
+            letter-spacing="0em"
+            >${title}
+            </p>
+    </foreignObject>
+    <text fill="#AEAEAE"  font-family="Helvetica" font-size="14" font-weight="bold" letter-spacing="0em"><tspan x="16" y="215">${hostname}</tspan></text>
+    <foreignObject
+          x="16" y=${coverImage !== "" ? `"370"` : `"225"`} width="260" height="120">
+          <p
+            xmlns="http://www.w3.org/1999/xhtml"
+            xlink:href="#blogName"
+            style="color:#AEAEAE;font-family:Helvetica;font-size:14px;margin:0;padding:0;font-weight=bold"
+            letter-spacing="0em"
+            >${brief}
+            </p>
+    </foreignObject>
+    ${coverImage !== "" ? `<rect id="coverImage" x="16" y="225" width="260" height="151.2" rx="4"/>
+    <clipPath id="coverImageClip">
+    <use xlink:href="#coverImage"/>
+    </clipPath>
+    <image x="16" y="225" width="260" clip-path="url(#coverImageClip)" height="151.2" preserveAspectRatio="xMinYMin" xlink:href="data:image/png;base64,${coverImage}"/>` : null}
+    </a >
+    </svg > `;
+  }
+  return `<svg  ${large ? ` width="302" height="522"` : ` width="174" height="300" `} viewBox="0 0 302 522" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <a href="${blogLink}" target="_blank">
   <rect x="1" y="1" width="300" height="520" rx="4" fill="white" stroke="#E5E7EB" stroke-width="2"/>
   <rect id="profileImage" x="16" y="20" width="40" height="40" rx="20" />
