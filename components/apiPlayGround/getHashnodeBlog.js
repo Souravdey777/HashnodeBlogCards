050d82d6d5147ffa3d6d9a18ef93db5038f97255
Souravdey777 <piyush.kolkata@gmail.com>
2021-02-02 22:50:39 +0530

    ⚗ svg in React DOM

--- components/apiPlayGround/getHashnodeBlog.js
+++ components/apiPlayGround/getHashnodeBlog.js
@@ -9,11 +9,18 @@ function GetHashnodeBlog(props) {
         console.log("clicked")
         return await axios
             .get(`https://hashnode-blog-cards.souravdey777.vercel.app/api/getHashnodeBlog?url=${BlogURL}&large=true`)
-            .then(response => {
-                console.log(response.data)
-                let blob = new Blob([response.data], { type: 'image/svg+xml' })
-                let url = URL.createObjectURL(blob);
-                setResponse(url);
+            .then(res => {
+                console.log(res.data)
+                // let blob = new Blob([response.data], { type: 'image/svg+xml' })
+                // let url = URL.createObjectURL(blob);
+                // setResponse(url);
+                // var parse = new DOMParser();
+                // var url = parse.parseFromString(res)
+                // setResponse(res.data);
+                const buff = new Buffer(res.data);
+                const base64data = buff.toString('base64');
+                console.log(typeof (res.data))
+                setResponse(base64data);
             })
     }
     const handleBlogURL = (event) => {
@@ -27,13 +34,13 @@ function GetHashnodeBlog(props) {
                     placeholder="Enter the URL..."
                     type="text" value={BlogURL} onChange={(e) => handleBlogURL(e)} />
                 {console.log(BlogURL)}
-
                 <div className={styles.sendRequest} onClick={() => getData()}>
                     Send 🚀
                 </div>
             </div>
-            <div>
-                {Response ? <img src={Response} /> : null}
+            <div className={styles.responseHolder}>
+                {Response ? <img src={`data:image/svg+xml;base64,${Response}`} /> : null}
+                {/* <img src={`https://hashnode-blog-cards.souravdey777.vercel.app/api/getLatestHashnodeBlog?username=${BlogURL}&large=true&limit=6`} /> */}
             </div>
         </div>
 
