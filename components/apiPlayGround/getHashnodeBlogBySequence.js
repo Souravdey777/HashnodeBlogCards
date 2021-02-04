96416d9e1aa4a6eb94b8edda941325cc3f5c975d
Souravdey777 <piyush.kolkata@gmail.com>
2021-02-04 19:30:12 +0530

    ✨ getHashnodeBlogBySequence added in playground

--- components/apiPlayGround/getHashnodeBlogBySequence.js
+++ components/apiPlayGround/getHashnodeBlogBySequence.js
@@ -1,11 +1,118 @@
-import React from 'react';
-import axios from 'axios';
+import styles from "../../styles/PlayGround.module.css";
+import { THEMES } from '../../utils/Constants';
+import * as loader from '../../assets/loader.json';
+import Lottie from "react-lottie";
 
 function GetHashnodeBlogBySequence(props) {
+    const loaderdefaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: loader.default,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
+    const { API_URL, setAPI_URL, params, setparams, isLoading, setisLoading } = props
+    const getData = () => {
+        console.log(API_URL)
+        if (`https://hashnode-blog-cards.souravdey777.vercel.app/api/getHashnodeBlogBySequence?username=${params.username}&sequence=${params.sequence}&large=${params.large}&theme=${params.theme}` !== API_URL) {
+            setisLoading(true)
+            console.log("SEND clicked");
+            setAPI_URL(`https://hashnode-blog-cards.souravdey777.vercel.app/api/getHashnodeBlogBySequence?username=${params.username}&sequence=${params.sequence}&large=${params.large}&theme=${params.theme}`);
+            console.log(API_URL);
+        }
+    }
+    const handleusername = (event) => {
+        setparams({ ...params, username: event.target.value });
+        console.log({ ...params, username: event.target.value })
+    }
+    const handlesequence = (event) => {
+        setparams({ ...params, sequence: event.target.value });
+        console.log({ ...params, sequence: event.target.value })
+    }
+    const handlelarge = (event) => {
+        setparams({ ...params, large: event.target.value });
+    }
+    const handletheme = (event) => {
+        setparams({ ...params, theme: event.target.value });
+    }
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(API_URL)
+    };
     return (
-        <div>
-            GetHashnodeBlogBySequence
+        <div className={styles.PlayGround}>
+            <div style={{ display: "flex", flexDirection: "column" }}>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <div className={styles.labels}>Enter the username</div>
+                    <input
+                        className={styles.textBox}
+                        placeholder="Enter the hashnode Blog Username..."
+                        type="text" value={params.username} onChange={handleusername} />
+                </div>
+                <div style={{ display: "flex", flexDirection: "row" }}>
+                    <div className={styles.labels}>Enter the sequence</div>
+                    <input
+                        className={styles.textBox}
+                        placeholder="Enter the Blog sequence..."
+                        type="number" min="1" value={params.sequence} onChange={handlesequence} />
+                </div>
+                <div className={styles.commonParams}>
+                    <div>
+                        <div className={styles.labels}>Choose Size</div>
+                        <select
+                            className={styles.textBox}
+                            value={params.large}
+                            onChange={handlelarge}
+                            placeholder="large size">
+                            <option value="true">large</option>
+                            <option value="false">small</option>
+                        </select>
+
+                    </div>
+                    <div>
+                        <div className={styles.labels}>Choose Theme</div>
+                        <select
+                            className={styles.textBox}
+                            value={params.theme}
+                            onChange={handletheme}
+                            placeholder="">.
+                            {THEMES.map((onetheme, key) =>
+                                <option key={key} value={onetheme.THEME_NAME}>{onetheme.THEME_NAME}</option>
+                            )}
+                        </select></div>
+                </div>
+                <div className={styles.sendRequest} onClick={() => getData()}>
+                    Send 🚀
+                </div>
+                <div className={styles.API_URL}>
+                    {
+                        <div onClick={() => copyToClipboard()} className={styles.copyIcon}><i style={{ fontSize: "16px" }} className="material-icons">content_copy</i>
+                            <span className={styles.tooltiptext}>Copy</span>
+                        </div>
+                    }
+                    {API_URL === "" ? "GET Request" : API_URL}
+                </div>
+            </div>
+            <div className={styles.labels}>Response Body</div>
+            <div className={styles.responseHolder}>
+                <object data={API_URL} style={{
+                    position: "absolute",
+                }} onLoad={() => { setisLoading(false) }}
+                    type="text/html"></object>
+                {isLoading ?
+                    <div className={styles.responseHolderLoader}>
+                        <Lottie
+                            options={loaderdefaultOptions}
+                            height={250}
+                            width={250}
+                            isStopped={false}
+                            isPaused={false}
+                            isClickToPauseDisabled={true}
+                        />
+                    </div> : null}
+            </div>
         </div>
+
     );
 }
 
--- components/playGround.js
+++ components/playGround.js
@@ -7,23 +7,39 @@ import { THEMES } from '../utils/Constants';
 function PlayGround(props) {
     const [isLoading, setisLoading] = useState(false)
 
-    const [API_URL, setAPI_URL] = useState("")
-    const [params, setparams] = useState({
+    //GetHashnodeBlog states
+    const [GetHashnodeBlog_API_URL, set_GetHashnodeBlog_API_URL] = useState("")
+    const [GetHashnodeBlog_params, set_GetHashnodeBlog_params] = useState({
         blogURL: "",
+        large: "false",
+        theme: THEMES[0].THEME_NAME
+    })
+
+    //GetHashnodeBlogBySequence states
+    const [GetHashnodeBlogBySequence_API_URL, set_GetHashnodeBlogBySequence_API_URL] = useState("")
+    const [GetHashnodeBlogBySequence_params, set_GetHashnodeBlogBySequence_params] = useState({
+        username: "",
+        sequence: "1",
         large: "false",
         theme: THEMES[0].THEME_NAME
     })
     if (props.Endpoint === 0) {
         return <GetHashnodeBlog
-            API_URL={API_URL}
-            setAPI_URL={setAPI_URL}
-            params={params}
-            setparams={setparams}
+            API_URL={GetHashnodeBlog_API_URL}
+            setAPI_URL={set_GetHashnodeBlog_API_URL}
+            params={GetHashnodeBlog_params}
+            setparams={set_GetHashnodeBlog_params}
             isLoading={isLoading}
             setisLoading={setisLoading} />
     }
     else if (props.Endpoint === 1) {
-        return <GetHashnodeBlogBySequence />
+        return <GetHashnodeBlogBySequence
+            API_URL={GetHashnodeBlogBySequence_API_URL}
+            setAPI_URL={set_GetHashnodeBlogBySequence_API_URL}
+            params={GetHashnodeBlogBySequence_params}
+            setparams={set_GetHashnodeBlogBySequence_params}
+            isLoading={isLoading}
+            setisLoading={setisLoading} />
     }
     else if (props.Endpoint === 2) {
         return <GetLatestHashnodeBlog />
